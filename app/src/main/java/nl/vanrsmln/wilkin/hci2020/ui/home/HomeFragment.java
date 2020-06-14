da114afcbb11e17097cdc1fd8ffb3bb42c76cde6
Sofia Konovalova <sophia.konovalova2@gmail.com>
2020-06-14 13:08:31 +0300

    basic card layout, scrollview doesn't work

--- app/src/main/java/nl/vanrsmln/wilkin/hci2020/ui/home/HomeFragment.java
+++ app/src/main/java/nl/vanrsmln/wilkin/hci2020/ui/home/HomeFragment.java
@@ -4,6 +4,7 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.GridLayout;
 import android.widget.TextView;
 import androidx.annotation.Nullable;
 import androidx.annotation.NonNull;
@@ -21,11 +22,11 @@ public class HomeFragment extends Fragment {
         homeViewModel =
                 ViewModelProviders.of(this).get(HomeViewModel.class);
         View root = inflater.inflate(R.layout.fragment_home, container, false);
-        final TextView textView = root.findViewById(R.id.text_home);
+        final GridLayout gridLayout = root.findViewById(R.id.home_cards);
         homeViewModel.getText().observe(this, new Observer<String>() {
             @Override
             public void onChanged(@Nullable String s) {
-                textView.setText(s);
+
             }
         });
         return root;
