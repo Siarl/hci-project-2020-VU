2aa6ccb5e8a35b98a024bceb34e5447c7a86838d
Wilkin van Roosmalen <w.s.roosmalen@gmail.com>
2020-06-15 18:43:05 +0200

    Remove fragments from MainActivity.java

--- app/src/main/java/nl/vanrsmln/wilkin/hci2020/MainActivity.java
+++ app/src/main/java/nl/vanrsmln/wilkin/hci2020/MainActivity.java
@@ -4,29 +4,48 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import androidx.cardview.widget.CardView;
-import android.view.View;
-import com.google.android.material.bottomnavigation.BottomNavigationView;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.navigation.NavController;
-import androidx.navigation.Navigation;
-import androidx.navigation.ui.AppBarConfiguration;
-import androidx.navigation.ui.NavigationUI;
+import com.google.android.material.snackbar.Snackbar;
+import nl.vanrsmln.wilkin.hci2020.repairs.ui.main.RepairActivity;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        BottomNavigationView navView = findViewById(R.id.nav_view);
-        AppBarConfiguration appBarConfiguration = new AppBarConfiguration.Builder(
-                R.id.navigation_home, R.id.navigation_dashboard, R.id.navigation_notifications)
-                .build();
+        CardView card = findViewById(R.id.associations_button);
+        CardView schedule = findViewById(R.id.schedule_button);
+        CardView lunch = findViewById(R.id.lunch_button);
+        CardView navigation = findViewById(R.id.navigation_button);
+        CardView repair = findViewById(R.id.repair_button);
+        CardView security = findViewById(R.id.security_button);
 
-        NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment);
-        NavigationUI.setupActionBarWithNavController(this, navController, appBarConfiguration);
-        NavigationUI.setupWithNavController(navView, navController);
+        card.setOnClickListener(this);
+        schedule.setOnClickListener(this);
+        security.setOnClickListener(this);
+        navigation.setOnClickListener(this);
+        repair.setOnClickListener(this);
+        lunch.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch(v.getId()) {
+            case R.id.associations_button:
+                startActivity(new Intent(this, AssociationsActivity.class));
+                break;
+            case R.id.security_button:
+                startActivity(new Intent(this, SecurityActivity.class));
+                break;
+            case R.id.repair_button:
+                startActivity(new Intent(this, RepairActivity.class));
+                break;
+            default:
+                Snackbar.make(v, "Unavailable in this version...stay tuned!", Snackbar.LENGTH_LONG).setAction("Action", null).show();
+                break;
+        }
     }
 
 }
